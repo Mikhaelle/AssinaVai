7dfee8be529a9ae3a75ec572702bde83b27c809f
Mikhaelle <mikhabueno@yahoo.com>
2019-09-26 16:48:09 -0300

    correct bugs and funcionalities

---
+++ client/src/components/Reactpdf.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { Document, Page } from 'react-pdf';
+ 
+class Reactpdf extends Component {
+  state = {
+    numPages: null,
+    pageNumber: 1,
+  }
+  onDocumentLoadSuccess = ({ numPages }) => {
+    this.setState({ numPages });
+  }
+ 
+  render() {
+    const { pageNumber, numPages } = this.state;
+ 
+    return (
+      <div>
+        <Document
+          onLoadSuccess={this.onDocumentLoadSuccess}
+        >
+          <Page pageNumber={pageNumber} />
+        </Document>
+        <p>Page {pageNumber} of {numPages}</p>
+      </div>
+    );
+  }
+}
+
+export default Reactpdf;
